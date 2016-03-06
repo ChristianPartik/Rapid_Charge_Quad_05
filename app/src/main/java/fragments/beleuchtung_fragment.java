f45080a532e2253165165cc50d907058992e43be
ChristianPartik <christian.partik@gmail.com>
2016-03-06 21:50:24 +0100

    progressbar and image assest

--- app/src/main/java/fragments/beleuchtung_fragment.java
+++ app/src/main/java/fragments/beleuchtung_fragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.christian.rapid_charge_quad_05.R;
 
@@ -14,11 +16,26 @@ import com.example.christian.rapid_charge_quad_05.R;
  */
 public class beleuchtung_fragment extends Fragment {
 
+    int Akkustand;
+
+    private ProgressBar progressBar;
+    private TextView textView;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.fragment_beleuchtung, container, false);
 
         return rootview;
+    }
+
+    private void Edit_Akkustand() {
+
+        progressBar.findViewById(R.id.circle_progress_bar);
+        progressBar.setProgress(Akkustand);
+
+        textView.findViewById(R.id.compliance_percentage);
+        textView.setText(Integer.toString(Akkustand) + "%");
     }
 }
