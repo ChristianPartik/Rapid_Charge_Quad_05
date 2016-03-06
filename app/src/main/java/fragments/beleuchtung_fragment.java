5cafbd341d8bcb46cb53a29c0c679c112969f4c8
ChristianPartik <christian.partik@gmail.com>
2016-03-06 21:08:34 +0100

    Initial commit

---
+++ app/src/main/java/fragments/beleuchtung_fragment.java
@@ -0,0 +1,24 @@
+package fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.christian.rapid_charge_quad_05.R;
+
+/**
+ * Created by Christian on 06.03.2016.
+ */
+public class beleuchtung_fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootview = inflater.inflate(R.layout.fragment_beleuchtung, container, false);
+
+        return rootview;
+    }
+}
